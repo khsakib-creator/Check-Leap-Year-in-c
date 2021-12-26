deab6e0e6ea1e241398a18dfc83a49e79b0f6159
khsakib-creator <75521595+khsakib-creator@users.noreply.github.com>
2021-12-26 23:00:16 +0600

    khsakib-creatorCheck-Leap-Year
    This is made by c in leap year calculator.

---
+++ khsakib-creatorCheck-Leap-Year.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+int main() {
+   int year;
+   printf("Enter a year: ");
+   scanf("%d", &year);
+   if (year % 400 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   else if (year % 100 == 0) {
+      printf("%d is not a leap year.", year);
+   }
+   else if (year % 4 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   else {
+      printf("%d is not a leap year.", year);
+   }
+}
